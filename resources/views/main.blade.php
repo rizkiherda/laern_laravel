f83d3a30d02394ab84eb0075a47cd96774d1eadf
RIZKI-LAPTOP\Rizki Herdatullah <rizkiherda@gmail.com>
2016-09-03 03:58:15 +0700

    Part 7 - Partials with Blade

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,84 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        @include('partials._head')
+    </head>
 
-<head>
+    <body>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Laravel Blog</title>
-    <!-- CHANGE THIS TITLE FOR EACH PAGE -->
+    @include('partials._nav')
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <div class="container">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="{{asset('js/html5shiv.min.js')}}"></script>
-    <script src="{{asset('js/respond.min.js')}}"></script>
-    <![endif]-->
-</head>
+        @yield('content')
 
-<body>
-
-<!-- Default Bootstrap Navbar -->
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Laravel Blog</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false">My Account <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
+        @include('partials._footer')
     </div>
-    <!-- /.container-fluid -->
-</nav>
 
-<div class="container">
+    @include('partials._javascript')
 
-    @yield('content')
-
-    <hr>
-
-    <p class="text-center">Copyright Rizki Herdatullah - all Rights Reserved</p>
-</div>
-<!-- end of .container -->
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="{{asset('js/jquery-3.1.0.min.js')}}"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="{{asset('js/bootstrap.min.js')}}"</script>
-
-</body>
+    @yield('scripts')
+    </body>
 
 </html>
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+
+<p class="text-center">Copyright Rizki Herdatullah - all Rights Reserved</p>
